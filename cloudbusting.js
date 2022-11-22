f84d93af869da264e1236477f0a57e455ff9a116
NimiphiG <115160805+NimiphiG@users.noreply.github.com>
2022-11-22 16:43:37 +0000

    22/11/22

--- cloudbusting.js
+++ cloudbusting.js
@@ -4,17 +4,31 @@
 let clouds = []
 
 let currentCloudTotal = 0
-let sky = document.getElementById('sky')
 
 //move through window on click
+document.getElementById('window1').addEventListener('mouseup', addmovethrough, generateCloud)
 function addmovethrough() {
+
   document.getElementById("window1").classList.add("move-through");
   document.getElementById("room").classList.add("move-through");
+  document.getElementById('window1').removeEventListener('click', addmovethrough)
+}
+document.getElementById('window1').addEventListener('animationend', shiftZ)
+function shiftZ() {
+  document.getElementById('sky').style.zIndex = 1
+  document.getElementById('machine').style.zIndex = 1
+  document.getElementById('hills').style.zIndex = 1
+  document.getElementById('key').style.zIndex = 1
+
 }
 
+//on scroll
 
 //rotate machine with mouse move both x and y
-window.addEventListener("mousemove", function mousemove(e) {
+window.addEventListener("mousemove", mousemove)
+
+ function mousemove(e) {
+  
   const machine = document.querySelector("#machine")
 
   var b = ((e.x) - 1050)
@@ -24,36 +38,36 @@ window.addEventListener("mousemove", function mousemove(e) {
 
   machine.style.rotate = -rot - 45 + "deg";
 
-
-
-})
-
-
+}
 
 //generate clouds on load not working
+window.onload = generateCloud()
 function generateCloud() {
-  let newElement = document.createElement('div');
+  for (let i = currentCloudTotal; i <= 20; i++) {
 
-  newElement.className = "cloud"
-  newElement.id = "cloud" + currentCloudTotal
-  newElement.style.left = (randomX) + "px"
-  newElement.style.top = (randomY) + "px"
-  clouds.push(newElement)
-  document.getElementById("sky").appendChild(newElement)
 
-  console.log('random run')
-  console.log(randomX)
+    let newElement = document.createElement('div');
 
-  let randomX = getrandomx()
-  function getrandomx() {
-    Math.floor(Math.random() * 1000)
+    let randomX = Math.floor(Math.random() * visualViewport.width);
+    let randomY = Math.floor(Math.random() * ((visualViewport.height / 100) * 50));
+
+    clouds.push(newElement)
+    let cloudIndex = clouds.indexOf(newElement)
+    newElement.className = "cloud"
+    newElement.id = cloudIndex      //number the clouds
+    newElement.style.left = (randomX) + "px";
+    newElement.style.top = (randomY) + "px";
+
+    let cloud = document.getElementById("sky").appendChild(newElement)
+    cloud.addEventListener('click', bustcloud);
+
   }
-
 }
 
 
+
 // mouse function
-window.addEventListener('mousedown',
+document.getElementById('sky').addEventListener('mousedown',
   function cloudMechanics(event) {
     if (event.ctrlKey) {
       console.log('add cloud')
@@ -61,39 +75,62 @@ window.addEventListener('mousedown',
 
     } else {
       console.log('cloudbusted')
-      cloudbust()
+      // cloudbust()
     }
   })
 
+
 // create cloud on click
 function createcloud(event) {
+
   let newElement = document.createElement('div');
 
+  let cloudIndex = clouds.indexOf(newElement)
   newElement.className = "cloud"
-  newElement.id = "cloud" + currentCloudTotal       //number the clouds?
+  newElement.id = cloudIndex      //number the clouds
   newElement.style.left = (event.clientX - 25) + "px"
   newElement.style.top = (event.clientY - 15) + "px"
 
-  clouds.push(newElement)
+  let cloud = document.getElementById("sky").appendChild(newElement)
 
-  document.getElementById("sky").appendChild(newElement)
+  cloud.addEventListener('click', bustcloud,)
 }
+
+//remove specific cloud and add raindrop
+function bustcloud(event) {
+  let cloud = clouds.splice((event.target.id - 1), 1)
+
+  if (cloud != null) {
+    document.getElementById("sky").removeChild(event.target)
+  }
+  console.log('make rain')
+  let newElement = document.createElement('div');
+  newElement.className = 'rain fall'
+  newElement.style.left = (event.clientX - 15) + "px"
+  newElement.style.top = (event.clientY - 15) + "px"
+  document.getElementById('sky').appendChild(newElement)
+  document.getElementsByClassName('fall').style.animation = 'fall' + time + 's' + 'linear'
+}
+
+
+
+//when all clouds are gone do thing 
+
+while (clouds.length === 0) {
+
+}
+
+
+
 
 // remove first cloud from array
-function cloudbust() {
-  let cloud = clouds.shift()
-  console.log(cloud)
-  if (cloud != null) {
-    document.getElementById("sky").removeChild(cloud)
-  }
-}
+// function cloudbust() {
+//   let cloud = clouds.shift()
 
-
-
-
-
-
-
+//   if (cloud != null) {
+//     document.getElementById("sky").removeChild(cloud)
+//   }
+// }
 
 
 // ROTATIOON ATTEMPTS
